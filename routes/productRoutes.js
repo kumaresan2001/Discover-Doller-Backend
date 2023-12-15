fad726c632d22c6f2ab809680882b4a135d7edf8
kumaresan2001 <kumaresancrseven@gmail.com>
2023-12-15 13:26:41 +0530

    new

---
+++ routes/productRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+  getProductDetails,
+  getProducts,
+} from "../controllers/productControllers.js";
+
+const router = express.Router();
+
+router.get("/products", getProducts);
+router.get("/product/:id", getProductDetails);
+
+export default router;
